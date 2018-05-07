1cabeb5974f44e2cb29c2068224ca3a66b1b7977
Pepe Becker <mail@pepebecker.com>
2018-05-07 23:19:25 +0100

    Add Zhuyin to Pinyin support

--- index.js
+++ index.js
@@ -3,23 +3,81 @@
 const pinyinUtils = require('pinyin-utils')
 const pinyinSplit = require('pinyin-split')
 const py2zh = require('./py2zh')
-// const zh2py = require('./zh2py')
+
+const zhuyinTones = ["", "ˊ", "ˇ", "`", "˙"]
 
 const fromPinyin = input => {
   const translate = pinyin => pinyinSplit(pinyin)
   .then(list => list.map(py => {
     let zh = py2zh[pinyinUtils.removeTone(py)]
-    return zh + py2zh[pinyinUtils.getToneNumber(py)]
+    return zh + zhuyinTones[pinyinUtils.getToneNumber(py) - 1]
   }))
   if (typeof input === 'string') return translate(input)
   else return Promise.all(input.map(translate))
 }
 
-const toPinyin = zhuyin => {
-  // TODO
-  return Promise.resolve(zhuyin)
+const splitZhuyin = zhuyin => {
+  const list = []
+  let index = 0
+  while (index < zhuyin.length) {
+    let count = zhuyin.length - index
+    let wordFound = false
+    while (count > 1) {
+      let word = zhuyin.substr(index, count)
+      if (Object.values(py2zh).includes(word)) { // word found
+        wordFound = true
+
+        if (zhuyinTones.includes(zhuyin[index + count])) { // tone found after word
+          word += zhuyin[index + count]
+          count ++
+        }
+
+        list.push([word])
+        index += count - 1
+        break
+      }
+      count --
+    }
+
+    if (!wordFound) {
+      if (index === 0 || typeof list[list.length - 1] === 'object') {
+        list.push(zhuyin[index])
+      } else if (typeof list[list.length - 1] === 'string') {
+        list[list.length - 1] += zhuyin[index]
+      }
+    }
+
+    index ++
+  }
+  return list
+}
+
+const toPinyin = (zhuyin, opts = {}) => {
+  let list = splitZhuyin(zhuyin)
+  if (opts.onlyPinyin) list = list.filter(item => Array.isArray(item))
+  list = list.map(item => {
+    if (typeof item === 'string') return item
+    let word = item[0]
+    let tone = zhuyinTones.indexOf(word[word.length - 1]) + 1
+    if (tone > 0) {
+      word = word.substr(0, word.length - 1)
+    } else {
+      tone = 1
+    }
+
+    let pinyinIndex = Object.values(py2zh).indexOf(word)
+    if (pinyinIndex > -1) {
+      const pinyin = Object.keys(py2zh)[pinyinIndex] + tone
+      if (opts.numbered) return pinyin
+      else return pinyinUtils.numberToMark(pinyin)
+    } else {
+      return item[0]
+    }
+  })
+  return Promise.resolve(list.join(opts.onlyPinyin ? ' ' : ''))
 }
 
 module.exports = fromPinyin
 module.exports.fromPinyin = fromPinyin
 module.exports.toPinyin = toPinyin
+module.exports.split = splitZhuyin
--- test/index.js
+++ test/index.js
@@ -12,3 +12,22 @@ describe('Convert Pinyin to Zhuyin', () => {
 		.then(list => list.join(' ').should.equal('ㄨㄛˇ ㄉㄜ˙ ㄇㄠ ㄒㄧˇ ㄏㄨㄢ˙ ㄏㄜ ㄋㄧㄡˊ ㄋㄞˊ'))
 	})
 })
+
+describe('Convert Zhuyin to Pinyin', () => {
+	it('should convert Zhuyin to Pinyin', () => {
+		return zhuyin.toPinyin('ㄨㄛˇ ㄉㄜ˙ ㄇㄠ ㄒㄧˇㄏㄨㄢ˙ ㄏㄜ ㄋㄧㄡˊㄋㄞˊ')
+		.then(pinyin => pinyin.should.equal('wǒ de māo xǐhuan hē niúnái'))
+	})
+	it('should convert Zhuyin to Pinyin', () => {
+		return zhuyin.toPinyin('ㄨㄛˇ ㄉㄜ˙ ㄇㄠ ㄒㄧˇㄏㄨㄢ˙ ㄏㄜ ㄋㄧㄡˊㄋㄞˊ', { numbered: true })
+		.then(pinyin => pinyin.should.equal('wo3 de5 mao1 xi3huan5 he1 niu2nai2'))
+	})
+	it('should convert Zhuyin to Pinyin', () => {
+		return zhuyin.toPinyin('My: "ㄨㄛˇ ㄉㄜ˙" cat: "ㄇㄠ" likes: "ㄒㄧˇㄏㄨㄢ˙" to drink: "ㄏㄜ" milk: "ㄋㄧㄡˊㄋㄞˊ"', { numbered: true, onlyPinyin: true })
+		.then(pinyin => pinyin.should.equal('wo3 de5 mao1 xi3 huan5 he1 niu2 nai2'))
+	})
+	it('should convert Zhuyin to Pinyin', () => {
+		return zhuyin.toPinyin('My: "ㄨㄛˇㄉㄜ˙" cat: "ㄇㄠ" likes: "ㄒㄧˇㄏㄨㄢ˙" to drink: "ㄏㄜ" milk: "ㄋㄧㄡˊㄋㄞˊ"', { numbered: true })
+			.then(pinyin => pinyin.should.equal('My: "wo3de5" cat: "mao1" likes: "xi3huan5" to drink: "he1" milk: "niu2nai2"'))
+	})
+})
